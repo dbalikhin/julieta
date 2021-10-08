2416ef0c316f6ee55220b420351630d66b31c0d8
Dmitry <dmodeus@gmail.com>
2021-10-08 18:05:45 -0400

    boom

--- julieta/Controllers/TestController.cs
+++ julieta/Controllers/TestController.cs
@@ -1,3 +1,5 @@
+﻿using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using julieta.Data;
@@ -18,7 +20,6 @@ namespace julieta.Controllers
         {
             _context = context;
         }
-        
         public IActionResult EfCoreSelectTest(string user)
         {
 
@@ -42,6 +43,24 @@ namespace julieta.Controllers
             var a5 = _context.Accounts
                 .FromSqlRaw("SELECT * FROM ACCOUNTS WHERE login = {0}", user)
                 .FirstOrDefault();
+
+            return new OkResult();
+        }
+
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA3001:Review code for SQL injection vulnerabilities", Justification = "<Pending>")]
+        public IActionResult SqlTest(string productName)
+        {
+            int productNameId = int.Parse(productName);
+            using (SqlConnection connection = new SqlConnection("dummyconnectionstring"))
+            {
+                SqlCommand sqlCommand = new SqlCommand()
+                {
+                    CommandText = "SELECT ProductId FROM Products WHERE ProductName = '" + productNameId + "'",
+                    CommandType = CommandType.Text,
+                };
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+            }
 
             return new OkResult();
         }
