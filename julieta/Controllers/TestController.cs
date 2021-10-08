6abc09a194d3e6392271ad711ced81b556b8200f
Dmitry <dmitry@andliza.com>
2021-10-08 16:20:59 -0400

    Update TestController.cs

--- julieta/Controllers/TestController.cs
+++ julieta/Controllers/TestController.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+using System.Linq;
+using System.Net;
 using julieta.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,27 +21,45 @@ namespace julieta.Controllers
         public IActionResult EfCoreSelectTest(string user)
         {
 
-            var account = _context.Accounts
+            var a1 = _context.Accounts
                 .FromSqlRaw("SELECT * FROM ACCOUNTS WHERE login={0}", user)
                 .FirstOrDefault();
 
             var sql = "SELECT * FROM ACCOUNTS WHERE login = '" + user + "'";
-            var account2 = _context.Accounts
+            var a2 = _context.Accounts
                 .FromSqlRaw(sql)
                 .FirstOrDefault();
 
-            var account3 = _context.Accounts
+            var a3 = _context.Accounts
                 .FromSqlRaw($"SELECT * FROM ACCOUNTS WHERE login = '{user}'")
                 .FirstOrDefault();
 
-            var account4 = _context.Accounts
+            var a4 = _context.Accounts
                 .FromSqlRaw("SELECT * FROM ACCOUNTS WHERE login = '" + user + "'")
                 .FirstOrDefault();
 
-            var account5 = _context.Accounts
+            var a5 = _context.Accounts
                 .FromSqlRaw("SELECT * FROM ACCOUNTS WHERE login = {0}", user)
                 .FirstOrDefault();
 
+            return new OkResult();
+        }
+
+        public IActionResult CookieTest()
+        {
+            Response.Cookies.Append(
+                "COOKIE_NAME",
+                "COOKIE_VALUE",
+                new CookieOptions()
+                {
+                    Path = "/",
+                    HttpOnly = false,
+                    Secure = false
+                }
+            );
+
+            Cookie cookie = new Cookie("SecretCookie", "SecretValue");
+            
             return new OkResult();
         }
     }
