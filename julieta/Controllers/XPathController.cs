8ca97496d0f503100354ad777e1ff4383bf5ec43
Dmitry <dmodeus@gmail.com>
2021-11-15 16:09:15 -0500

    meh

--- julieta/Controllers/XPathController.cs
+++ julieta/Controllers/XPathController.cs
@@ -2,6 +2,7 @@
 using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.XPath;
+using System.Xml.Xsl;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,7 +18,7 @@ namespace julieta.Controllers
 
         public ActionResult SampleSonar1(string user, string pass)
         {
-            String expression = "/users/user[@name='" + user + "' and @pass='" + pass + "']"; // Unsafe
+            string expression = "/users/user[@name='" + user + "' and @pass='" + pass + "']"; // Unsafe
 
             // An attacker can bypass authentication by setting user to this special value
             // user = "' or 1=1 or ''='";
@@ -31,19 +32,13 @@ namespace julieta.Controllers
             if (!ValidateInputParameters(user, pass)) 
                 return NotFound();
 
-            String expression = "/users/user[@name='" + user + "' and @pass='" + pass + "']"; // Compliant
+            string expression = "/users/user[@name='" + user + "' and @pass='" + pass + "']"; // Compliant
             return Content(doc.SelectSingleNode(expression) != null ? "success" : "fail");
         }
 
         private bool ValidateInputParameters(string user, string pass)
         {
-            if (!Regex.IsMatch(user, "^[a-zA-Z]+$") || !Regex.IsMatch(pass, "^[a-zA-Z]+$"))
-            {
-                return false;
- 
-            }
-
-            return true;
+            return Regex.IsMatch(user, "^[a-zA-Z]+$") && Regex.IsMatch(pass, "^[a-zA-Z]+$");
         }
 
         public ActionResult SampleRoslyn(string operation)
@@ -78,6 +73,87 @@ namespace julieta.Controllers
             return Ok();
         }
 
+        public ActionResult SampleCodeQL(string userName)
+        {
+        // BAD: Use user-provided data directly in an XPath expression
+            string badXPathExpr = "//users/user[login/text()='" + userName + "']/home_dir/text()";
+            XPathExpression.Compile(badXPathExpr);
+
+            // GOOD: XPath expression uses variables to refer to parameters
+            string xpathExpression = "//users/user[login/text()=$username]/home_dir/text()";
+            XPathExpression xpath = XPathExpression.Compile(xpathExpression);
+
+            // Arguments are provided as a XsltArgumentList()
+            XsltArgumentList varList = new XsltArgumentList();
+            varList.AddParam("userName", string.Empty, userName);
+
+            // CustomContext is an application specific class, that looks up variables in the
+            // expression from the varList.
+            CustomContext context = new CustomContext(new NameTable(), varList);
+            xpath.SetContext(context);
+            return Ok();
+        }
+
+
+    }
+
+    public class CustomContext : XsltContext
+    {
+        public CustomContext(NameTable nt, XsltArgumentList argsList) : base(nt)
+        {
+        }
+
+        public override int CompareDocument(string baseUri, string nextbaseUri)
+        {
+            return 0;
+        }
+
+        public override bool PreserveWhitespace(System.Xml.XPath.XPathNavigator node)
+        {
+            return false;
+        }
+
+        public override IXsltContextFunction ResolveFunction(string prefix, string name, System.Xml.XPath.XPathResultType[] ArgTypes)
+        {
+            return name.Equals("current") ? new XPathContextFunction("current") : null;
+        }
+
+        public override IXsltContextVariable ResolveVariable(string prefix, string name)
+        {
+            return null;
+        }
+
+        public override bool Whitespace => false;
+
+        private class XPathContextFunction : IXsltContextFunction
+        {
+            private string _functionName;
+
+            public XPathContextFunction(string functionName)
+            {
+                _functionName = functionName;
+            }
+
+            public XPathResultType[] ArgTypes => null;
+
+            public XPathResultType ReturnType => XPathResultType.Navigator;
+
+            public int Minargs => 0;
+
+            public int Maxargs => 0;
+
+            public object Invoke(XsltContext xsltContext, object[] args, XPathNavigator docContext)
+            {
+                if (_functionName.Equals("current"))
+                {
+                    XmlNode currentNode = ((IHasXmlNode)docContext).GetNode();
+
+                    return currentNode;
+                }
+
+                return null;
+            }
+        }
     }
 
 
